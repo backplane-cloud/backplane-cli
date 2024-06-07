40079165f7a7447a6d038b10594ba98d1faf95ef
Lewis Sheridan <lewis@backplane.cloud>
2024-06-07 13:29:26 +0100

    Added bp console open command to launch UI

--- bin/bp.js
+++ bin/bp.js
@@ -4,23 +4,23 @@ const program = require("commander");
 const pkg = require("../package.json");
 const colors = require("colors");
 program
+  .command("", `\n BACKPLANE CLI (v${pkg.version})`.yellow)
   .version(`${pkg.version}`)
 
-  .command("", "\n----------[ Developer ]----------")
+  .command("", "\n".yellow)
   .command("app", "Manage Apps".yellow)
+  .command("template", "Configure Environment App Templates")
   .command("service", "Publish to Integration Services Catalog".yellow)
 
-  .command("", "\n----------[ Administration ]----------")
+  .command("", "\n")
   .command("product", "Manage Products")
   .command("platform", "Manage Platforms")
   .command("org", "Manage Orgs")
-  .command("request", "Manage Approval Requests")
-  .command("backlog", "Manage Product Backlog, Sprints and Items")
   .command("cloud", "Register Cloud Service Provider")
-  .command("template", "Configure Environment App Templates")
   .command("budget", "Manage Organisational Budget")
+  .command("request", "Manage Approval Requests")
 
-  .command("", "\n-----------[ Access Management ]--------")
+  .command("", "\n")
   .command("auth", "Login to Backplane".yellow)
   .command("token", "Manage API JWT Token".yellow)
   .command("user", "Manage Users".cyan)
@@ -28,24 +28,20 @@ program
   .command("assign", "Manage RBAC Assignments".cyan)
   .command("role", "Manage Roles and Role Actions".cyan)
 
-  .command("", "\n----------[ Cloud Governance] ----------")
-  .command("cost", "Cost Data for Org, Platform, Products and Apps".gray)
-  .command("policy", "Policy Management".green)
-  .command("access", "Access Management".green)
-  .command("resource", "Resource Explorer".gray)
+  .command("", "\n".gray)
+  .command("gem", "General Exemption Manager".gray)
+  .command("slim", "Software License Inventory Manager".gray)
+  .command("game", "General Access Management Engine".gray)
+  .command("roi", "Resource Optimisation Insights".gray)
+  .command("oopa", "Open Onboarding Platform API".gray)
+  .command("graph", "Cloud Graph".gray)
+  .command("what", "Who has access to What ?".gray)
+  .command("backlog", "Manage Product Backlog, Sprints and Items".gray)
 
-  // .command("", "\n----------[ Extensions ]----------".gray)
-  // .command("gem", "General Exemption Manager".gray)
-  // .command("slim", "Software License Inventory Manager".gray)
-  // .command("game", "General Access Management Engine".gray)
-  // .command("roi", "Resource Optimisation Insights".gray)
-  // .command("oopa", "Open Onboarding Platform API".gray)
-  // .command("graph", "Cloud Graph".gray)
-  // .command("what", "Who has access to What ?".gray)
-
-  .command("", "\n----------[ Documentation ]----------")
+  .command("", "\n")
   .command("getting-started", "Getting Started User Guide")
   .command("about", "About Backplane")
+  .command("console", "Launch Backplane Console")
 
   .command("", "\n")
 
